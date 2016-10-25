8ddfb822daa857fce9af2666019148c297b989c9
turneye <marcus@turncode.productions>
2016-10-26 10:41:07 +1100

    final version with registration

--- PublicWebsite/PublicWebsite/App/app.js
+++ PublicWebsite/PublicWebsite/App/app.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', [
+    var app = angular.module('app', [
         // Angular modules
         'ngAnimate',
         'ngRoute'
@@ -11,4 +11,82 @@
         // 3rd Party Modules
         
     ]);
+
+
+    app.controller('RegisterController', ['$http', '$window', function ($http, $window) {
+        var vm = this;
+
+        vm.formData = {};
+
+        vm.modules = [{ name: 'Car / Ride Sharing' },
+                { name: 'Energy Sharing' },
+                { name: 'Participatory Democracy' },
+                { name: 'Connecting with Neighbours' },
+                { name: 'Stuff Sharing / Swapping' },
+                { name: 'Finance / Lending' },
+                { name: 'Reputation and Trust' }];
+
+        vm.toggleSelected = function (item) {
+            item.selected = !(item.selected);
+        };
+
+        vm.sendEmail = function () {
+            if (vm.formData.name == null || vm.formData.name == "" || vm.formData.email == null || vm.formData.email == "") {
+                $window.scrollTo(0, 0);
+                return;
+            }
+            vm.formData.interests = [];
+            for (var i = 0; i < vm.modules.length; i++) {
+                if (vm.modules[i].selected) vm.formData.interests.push(vm.modules[i].name);
+            }
+            $http.post('/api/Email/', vm.formData).
+                    then(function (response) {
+                        vm.success = true;
+                        $window.scrollTo(0, 0);
+                    }, function (response) {
+                        vm.error = true;
+                    });
+        };
+
+        vm.lastAmountSelected = -1;
+        vm.clearpledge = function (amt) {
+            if (amt == vm.lastAmountSelected) {
+                vm.lastAmountSelected = null;
+                vm.formData.pledgeAmount = null;
+            } else {
+                vm.lastAmountSelected = amt;
+            }
+        };
+
+    }]);
+
+
+    app.controller('LearnController', [function () {
+        var vm = this;
+        //{author: "xxxx", link: "xxxx", title: "xxxx"}
+        vm.whatis = [
+            {author: "A Shareable Explainer", link: "http://www.shareable.net/blog/a-shareable-explainer-what-is-a-platform-co-op", title: "What is a platform co-op"},
+            {author: "Trebor Shultz", link: "https://medium.com/@trebors/platform-cooperativism-vs-the-sharing-economy-2ea737f1b5ad#.wbl4krcpq", title: "Platform Cooperativism vs The Sharing Economy"},
+            {author: "Nathan Schneider", link: "http://www.shareable.net/blog/owning-is-the-new-sharing", title: "Owning is the new sharing"},
+            {author: "Janele Orsi", link: "http://bccm.coop/the-bccm-at-the-sydney-vivid-festival/#.WA2E36Or1E4", title: "BCCM at the Sydney Vivid Festival"},
+            {author: "Platform Cooperative", link: "http://platformcoop.net", title: "Platform Cooperativism 2016 Conference"}
+        ];
+
+        vm.economy = [
+            {author: "Rachel Bostman", link: "https://www.ted.com/talks/rachel_botsman_the_case_for_collaborative_consumption", title: "The case for collaborative consumption"},
+            {author: "Rachel Botsman", link: "https://www.ted.com/talks/rachel_botsman_the_currency_of_the_new_economy_is_trust?language=en", title: "On trust as the currency of the new economy"},
+            {author: "Robin Chase on Peers Inc", link: "http://www.businessinnovationfactory.com/summit/video/robin-chase-reinventing-capitalism#.WA2FpKOr1E4", title: "Reinventing Capitalism"},
+            {author: "Jeremiah Owyang", link: "http://www.web-strategist.com/blog/2016/03/10/honeycomb-3-0-the-collaborative-economy-market-expansion-sxsw", title: "The collaborative economy market expansion"},
+            {author: "Ben Tarnoff", link: "http://www.theguardian.com/technology/2016/oct/17/sharing-economy-capitalism-uber-airbnb-ownership", title: "The future: where borrowing is the norm and ownership is luxury"}
+        ];
+
+        vm.talks = [
+            {author: "Douglas Rushkoff", link: "http://www.rushkoff.com/books/throwing-rocks-at-the-google-bus", title: "Throwing Rocks at the Google Bus (book)"},
+            {author: "Douglas Rushkoff", link: "https://www.youtube.com/watch?v=MFTGyvAwg5U", title: "How Growth Became the Enemy of Prosperity"},
+            {author: "Jose Ramos", link: "http://www.earthsharing.org.au/2016/02/the-city-as-a-commons", title: "The City as Commons"},
+            {author: "Jose Ramos", link: "https://blog.p2pfoundation.net/city-commons-policy-reader/2016/07/22", title: "The City as Commons: A Policy Reader"}
+        ];
+
+    }]);
+
 })();
---
+++ PublicWebsite/PublicWebsite/Controllers/EmailController.cs
@@ -0,0 +1,112 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Http;
+
+namespace AngularJSWebApiEmpty.Controllers
+{
+    public class EmailController : ApiController
+    {
+        public class RegistrationForm
+        {
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string otherinfo { get; set; }
+            public string skills { get; set; }
+            public bool ambassador1 { get; set; }
+            public bool ambassador2 { get; set; }
+            public List<string> interests { get; set; }
+            public int? pledgeAmount { get; set; }
+        }
+
+        //private SmtpClient GetSmtpClient()
+        //{
+
+        //    SmtpClient smtp = new SmtpClient("smtpout.asia.secureserver.net", 80);
+        //    smtp.UseDefaultCredentials = false;
+        //    smtp.Credentials = new NetworkCredential("support@ourriskplan.com", "Blues12345");
+        //    smtp.EnableSsl = false;
+        //    return smtp;
+        //}
+
+        // POST: api/Email
+        public IHttpActionResult Post(RegistrationForm form)
+        {
+
+            //MailMessage m = new MailMessage();
+            //m.From = new MailAddress(C_FROMEMAIL);
+            //m.Subject = "Volunteer Inductions";
+            //m.IsBodyHtml = true;
+
+
+            //m.To.Clear();
+            //m.To.Add(new MailAddress(item.EmailAddress, item.Firstname + " " + item.Surname));
+            //m.Body = "<div style='font-family: Calibri, Geneva, Sans'>" +
+            //         "Dear " + item.Firstname + ", <br/><br/>" +
+            //         "To complete your induction and view our Code of Conduct please use the link below.<br/><br/>" +
+            //         "<a href='" + link + "'>Code of Conduct</a><br/><br/>" +
+            //         "Thanks,<br/>" +
+            //         "<b>" + account.AccountName + "</b><br/>" +
+            //         "<i><small>Our Risk Plan</small></i>";
+            //GetSmtpClient().Send(m);
+
+
+            var body = "<body>bHive website - Register Your Interest form submitted<br/><br/>";
+            body += "Name : " + form.name + "<br/>";
+            body += "Email : " + form.email + "<br/>";
+            body += "Phone : " + form.phone + "<br/>";
+            body += "Other Info : " + form.otherinfo + "<br/>";
+            body += "<br/>";
+            if (form.skills != null) body += "Skills : " + form.skills + "<br/><br/>";
+
+            if (form.pledgeAmount != null)
+            {
+                if(form.pledgeAmount == 0)
+                {
+                    body += "Hmmmm, I'm sure what I'd like to pledge yet.<br/><br/>";
+                }
+                else
+                {
+                    body += "Pledged Amount : $" + form.pledgeAmount + (form.pledgeAmount == 2000 ? " (WOOHOO!)" : "") + "<br/><br/>";
+                }
+            }
+
+
+            if (form.ambassador1 || form.ambassador2) body += "As an ambassador :";
+            if (form.ambassador1) body += "<br/><b>I would like to talk to you about some ideas</b><br/>";
+            if (form.ambassador2) body += "<br/><b>I know someone who would be really interested in this</b><br/>";
+
+            if (form.interests != null && form.interests.Count > 0)
+            {
+                body += "<br/>I am interested in using bHive for : ";
+                form.interests.ForEach(o =>
+                {
+                    body += o + "  |  ";
+                });
+            }
+            body += "</body>";
+
+            Email from = new Email("info@bhive.coop");
+            string subject = "bHive : Expression of Interest";
+            Content content = new Content("text/html", body);
+            Email to = new Email("marcus@turncode.productions");
+            Mail mail = new Mail(from, subject, to, content);
+            
+            dynamic sg = new SendGridAPIClient(ConfigurationManager.AppSettings.Get("EmailAPI"));
+            sg.client.mail.send.post(requestBody: mail.Get());
+
+            to = new Email("ian@liveecological.com.au");
+            mail = new Mail(from, subject, to, content);
+            sg.client.mail.send.post(requestBody: mail.Get());
+
+            return Ok();
+        }
+
+    }
+}
